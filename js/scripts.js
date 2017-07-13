a1d7816be9009a056c5a9064ae9695e0e6f9b020
Andrew Parks and Robert Murray <andrew@>
2017-07-13 13:53:58 -0700

    work properly up 3999 add comments to explain the functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var numerals = ["I", "V", "X", "L", "C", "D", "M"];
 
+//endNumeral() goes through the array split from the input and calls on numI() to create a roman numeral string to return
 var endNumeral = function(array){
   var romanNumeral = "";
   for (var i = 0; i < array.length; i++) {
@@ -8,9 +9,9 @@ var endNumeral = function(array){
   return romanNumeral;
 }
 
+//numI() takes the digit at the array index and translates it into a roman numeral
 var numI = function(index, length, number) {
-  debugger;
-
+///////////////Initialize based on array length
   if (length === 1) {
     var newNumber = numerals[1];
     var minorNumber = numerals[0];
@@ -25,7 +26,42 @@ var numI = function(index, length, number) {
       var minorNumber = numerals[0];
       var majorNumber = numerals[2];
     }
-  }
+  } else if (length === 3) {
+    if (index === 0) {
+      var newNumber = numerals[5];
+      var minorNumber = numerals[4];
+      var majorNumber = numerals[6];
+    } else if (index === 1) {
+      var newNumber = numerals[3];
+      var minorNumber = numerals[2];
+      var majorNumber = numerals[4];
+    } else {
+      var newNumber = numerals[1];
+      var minorNumber = numerals[0];
+      var majorNumber = numerals[2];
+    }
+  } else if (length === 4) {
+    if (index === 0) {
+      var newNumber = "";
+      var minorNumber = numerals[6];
+      for (number; number != 0; number--) {
+        newNumber += minorNumber;
+      }
+      return newNumber;
+    } else if (index === 1) {
+      var newNumber = numerals[5];
+      var minorNumber = numerals[4];
+      var majorNumber = numerals[6];
+    } else if (index === 2) {
+      var newNumber = numerals[3];
+      var minorNumber = numerals[2];
+      var majorNumber = numerals[4];
+    } else {
+      var newNumber = numerals[1];
+      var minorNumber = numerals[0];
+      var majorNumber = numerals[2];
+    }
+  } ///////// End of initialize segment
   if (number === 4) {
     newNumber = minorNumber + newNumber;
     return newNumber;
@@ -47,71 +83,13 @@ var numI = function(index, length, number) {
     return newNumber;
   }
 }
-//////////////////////////////////////////////////
+
+//numberSeparator() separates the string of digits into an array of each individual digit
 var numberSeparator = function(numberString) {
   numberArray = numberString.split("");
   return numberArray;
 }
-/*
-if string.length > 1
-ex "15"
-create array [1, 5]
-array.length = 2
-call on functions for 10s and 1s
 
-or
-
-ex "132"
-create array [1,3,2]
-array.length = 3
-call on functions for 100s, 10s, 1s
-*/
-
-/*///////////////////////////////////////////////////////
-var numI = function(number) {
-  var newNumber = "V";
-  if (number === 4) {
-    newNumber = "I" + newNumber;
-    return newNumber;
-  } else if (number === 9) {
-    newNumber = "IX"
-    return newNumber;
-  } else if (number < 5) {
-    newNumber = "";
-    for (number; number != 0; number--) {
-      newNumber += "I";
-    }
-    return newNumber;
-  } else if (number > 5) {
-    for (number %= 5; number != 0; number--) {
-      newNumber += "I";
-    }
-    return newNumber;
-  } else {
-    return newNumber;
-  }
-}
-*/////////////////////////////////////////////////////
-
-
-
-/*
-1=I
-5=V
-10=X
-50=L
-100=C
-500=D
-1000=M
-
-add and subtract to get values
-
-5%3 = 2
-5%4 = 1
-
-19 = 10 + (10-1) = XIX
-99 = (100-10) + (10-1) XCIX
-*/
 $(function() {
   $("#changeNumber").submit(function(event) {
     event.preventDefault();
@@ -120,8 +98,5 @@ $(function() {
     console.log(inputArray);
 
     $("#output").text(endNumeral(inputArray));
-    //var roman = numI(parseInt($("#number").val()));
-
-    //$("#output").text(roman);
   });
 });
